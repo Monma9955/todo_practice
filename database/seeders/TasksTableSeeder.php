c5859d99c4b30bde206c1d0f37ae4275ef8e2ac0
Monma_Takeshi <jemini0527@gmail.com>
2020-12-05 08:29:13 +0900

    seederデータ作成
    taskのseederデータ作成。
    folder id 1に対して３つのtaskのサンプルデータを作成しました。

--- database/seeders/TasksTableSeeder.php
+++ database/seeders/TasksTableSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TasksTableSeeder extends Seeder
 {
@@ -13,6 +15,15 @@ class TasksTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (range(1, 3) as $num) {
+            DB::table('tasks')->insert([
+                'folder_id' => 1,
+                'title' => "サンプルタスク {$num}",
+                'status' => $num,
+                'due_date' => Carbon::now()->addDay($num),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
     }
 }
