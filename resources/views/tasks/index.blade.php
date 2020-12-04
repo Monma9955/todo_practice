4c70c5604022127c7cb125dd0c61298a2ca9f384
Monma_Takeshi <jemini0527@gmail.com>
2020-12-05 05:00:13 +0900

    フォルダーリスト クラス名分岐 index.blade.php
    リストのフォルダーIDがカレントIDと一致した場合はそのフォルダーのクラスがアクティブになるよう条件分岐を追加しました。

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -26,7 +26,8 @@
           </div>
           <div class="list-group">
             @foreach($folders as $folder)
-              <a href="{{ route('tasks.index', ['id' => $folder->id]) }}" class="list-group-item">
+              <a href="{{ route('tasks.index', ['id' => $folder->id]) }}"
+                class="list-group-item {{ $current_folder_id === $folder->id ? 'active' : ''}}">
                 {{ $folder->title }}
               </a>
             @endforeach
