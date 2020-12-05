6cedcffcd5a32158349510a90e1c0066f65d1e6d
Monma_Takeshi <jemini0527@gmail.com>
2020-12-05 13:40:19 +0900

    選択フォルダの関連タスクを表示 index.blade.php
    選択されたフォルダの関連タスクがフォルダの横に表示されるようビューを変更。

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -25,6 +25,7 @@
             </a>
           </div>
           <div class="list-group">
+            {{-- フォルダ一覧を表示 --}}
             @foreach($folders as $folder)
               <a href="{{ route('tasks.index', ['id' => $folder->id]) }}"
                 class="list-group-item {{ $current_folder_id === $folder->id ? 'active' : ''}}">
@@ -35,7 +36,39 @@
         </nav>
       </div>
       <div class="column col-md-8">
-        <!-- ここにタスクが表示される -->
+        <div class="panel panel-default">
+          <div class="panel-heading">タスク</div>
+          <div class="panel-body">
+            <div class="text-right">
+              <a href="#" class="btn btn-default btn-block">
+                タスクを追加する
+              </a>
+            </div>
+          </div>
+          <table class="table">
+            <thead>
+            <tr>
+              <th>タイトル</th>
+              <th>状態</th>
+              <th>期限</th>
+              <th></th>
+            </tr>
+            </thead>
+            <tbody>
+              {{-- タスク一覧を表示 --}}
+              @foreach($tasks as $task)
+                <tr>
+                  <td>{{ $task->title }}</td>
+                  <td>
+                    <span class="label">{{ $task->status }}</span>
+                  </td>
+                  <td>{{ $task->due_date }}</td>
+                  <td><a href="#">編集</a></td>
+                </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
