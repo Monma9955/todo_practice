35b97c5b87b1b0da66c99f8b744db4b9aeadf0ce
Monma_Takeshi <jemini0527@gmail.com>
2020-12-07 00:28:16 +0900

    ビュー変更 日付の表示形式変更  index.blade.php
    index.blade.phpのビューでタスクの日付をアクセサでフォーマットした日付が呼び出されるように変更しました。

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -62,7 +62,7 @@
                   <td>
                     <span class="label {{ $task->status_class }}">{{ $task->status_label }}</span>
                   </td>
-                  <td>{{ $task->due_date }}</td>
+                  <td>{{ $task->formatted_due_date }}</td>
                   <td><a href="#">編集</a></td>
                 </tr>
               @endforeach
