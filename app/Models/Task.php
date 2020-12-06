f2270be5e5bfd2c438e657c7e6503450d197a90e
Monma_Takeshi <jemini0527@gmail.com>
2020-12-06 20:18:21 +0900

    アクセサ追加 TaskModel
    Taskモデルにステータスを日本語の文字列で表記するアクセサを追加。
    ステータスが1〜3の時にそれぞれ未着手・着手中・完了と表現されるようにし、定義されていない番号の場合は空文字を返すようにしました。

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    // アクセサ
+    
+    // STATUSの状態を定義
+    const STATUS = [
+        1 => [ 'label' => '未着手' ],
+        2 => [ 'label' => '着手中' ],
+        3 => [ 'label' => '完了' ]
+    ];
+
+    public function getStatusLabelAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければ空文字を返す
+        if(!isset(self::STATUS[$status])) {
+            return '';
+        }
+        return self::STATUS[$status]['label'];
+    }
 }
