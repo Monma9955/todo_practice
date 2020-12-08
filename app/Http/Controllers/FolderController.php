d857f71cbe007aae2a9f9b3e147d9b12797d637a
Monma_Takeshi <jemini0527@gmail.com>
2020-12-09 03:08:50 +0900

    フォルダー追加処理 createアクション追加
    FolderControllerにフォルダー追加時の処理としてcreateアクションを作成しました。
    フォームの入力値を$folderに代入し、DBに保存。
    保存後はindexへリダイレクトするようにしました。

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -11,4 +11,19 @@ class FolderController extends Controller
     {
         return view('folders/create');
     }
+
+    // リクエストを引数「$request」として渡す
+    public function create(Request $request)
+    {
+        $folder = new Folder();
+        // フォルダーテーブルのtitleカラムに入力値のtitleを代入
+        $folder->title = $request->title;
+        // DBに保存
+        $folder->save();
+
+        // 作成したフォルダーIDで一覧に戻る
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id
+        ]);
+    }
 }
