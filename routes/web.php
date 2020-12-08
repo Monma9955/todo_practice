7142b1dded7744c24adc6bac96774efec9e2addf
Monma_Takeshi <jemini0527@gmail.com>
2020-12-08 16:42:38 +0900

    フォルダー新規作成機能の実装 ルーティング
    フォルダー新規作成機能の実装。
    フォルダー作成画面と投稿処理へのルーティングを記載。

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,5 @@
 <?php
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'FolderController@create');
