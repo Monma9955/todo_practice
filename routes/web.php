e4e941de5f9b6c38d9e2d37bb86511d1002d2846
Monma_Takeshi <jemini0527@gmail.com>
2020-12-10 20:48:49 +0900

    ルーティング web.php
    タスク新規作成機能に関わるルーティングを記述しました。

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,5 @@
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
+Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', 'TaskController@create');
