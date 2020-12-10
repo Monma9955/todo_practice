b3d3303bcd6f8a147885718c76159fddbedecfe5
Monma_Takeshi <jemini0527@gmail.com>
2020-12-10 21:08:17 +0900

    ビュー作成 tasks/create
    TaskControllerのcreateビューを作成。
    フォームでタイトルと期限が設定できるようにしました。

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ToDo App</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+  <link rel="stylesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+<header>
+  <nav class="my-navbar">
+    <a class="my-navbar-brand" href="/">ToDo App</a>
+  </nav>
+</header>
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-offset-3 col-md-6">
+        <nav class="panel panel-default">
+          <div class="panel-heading">タスクを追加する</div>
+          <div class="panel-body">
+            @if($errors->any())
+              <div class="alert alert-danger">
+                @foreach($errors->all() as $message)
+                  <p>{{ $message }}</p>
+                @endforeach
+              </div>
+            @endif
+            <form action="{{ route('tasks.create', ['id' => $folder_id]) }}" method="POST">
+              @csrf
+              <div class="form-group">
+                <label for="title">タイトル</label>
+                <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}" />
+              </div>
+              <div class="form-group">
+                <label for="due_date">期限</label>
+                <input type="text" class="form-control" name="due_date" id="due_date" value="{{ old('due_date') }}" />
+              </div>
+              <div class="text-right">
+                <button type="submit" class="btn btn-primary">送信</button>
+              </div>
+            </form>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+</main>
+</body>
+</html>
