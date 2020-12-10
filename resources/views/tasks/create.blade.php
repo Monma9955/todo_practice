e99942db45a336b1b30832f70fc1e7f2124a32f2
Monma_Takeshi <jemini0527@gmail.com>
2020-12-11 00:33:29 +0900

    ビューテンプレート共通化
    今までに作成したビューテンプレートで共通する部分をlayout.blade.phpにして、異なる部分をyieldで読み込ませるようにしました。

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,23 +1,11 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>ToDo App</title>
-  {{-- flatpickrのデフォルトのスタイルシート --}}
+@extends('layout')
+
+@section('styles')
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-  {{-- ブルーテーマの追加スタイルシート --}}
   <link rel="stylesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-<header>
-  <nav class="my-navbar">
-    <a class="my-navbar-brand" href="/">ToDo App</a>
-  </nav>
-</header>
-<main>
+@endsection
+
+@section('content')
   <div class="container">
     <div class="row">
       <div class="col col-md-offset-3 col-md-6">
@@ -50,18 +38,16 @@
       </div>
     </div>
   </div>
-</main>
+@endsection
 
-{{-- flatpickrスクリプト --}}
-<script src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js"></script>
-{{-- 日本語化のための追加スクリプト --}}
-<script src="https://npmcdn.com/flatpickr/dist/l10n/ja.js"></script>
-<script>
-  flatpickr(document.getElementById('due_date'), {
-    locale: 'ja',
-    dateFormat: "Y/m/d",
-    minDate: new Date()
-  });
-</script>
-</body>
-</html>
+@section('scripts')
+  <script src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js"></script>
+  <script src="https://npmcdn.com/flatpickr/dist/l10n/ja.js"></script>
+  <script>
+    flatpickr(document.getElementById('due_date'), {
+      locale: 'ja',
+      dateFormat: "Y/m/d",
+      minDate: new Date()
+    });
+  </script>
+@endsection
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>ToDo App</title>
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-<header>
-  <nav class="my-navbar">
-    <a class="my-navbar-brand" href="/">ToDo App</a>
-  </nav>
-</header>
-<main>
+@extends('layout')
+
+@section('content')
   <div class="container">
     <div class="row">
       <div class="col col-md-4">
@@ -25,10 +12,11 @@
             </a>
           </div>
           <div class="list-group">
-            {{-- フォルダ一覧を表示 --}}
             @foreach($folders as $folder)
-              <a href="{{ route('tasks.index', ['id' => $folder->id]) }}"
-                class="list-group-item {{ $current_folder_id === $folder->id ? 'active' : ''}}">
+              <a
+                  href="{{ route('tasks.index', ['id' => $folder->id]) }}"
+                  class="list-group-item {{ $current_folder_id === $folder->id ? 'active' : '' }}"
+              >
                 {{ $folder->title }}
               </a>
             @endforeach
@@ -55,23 +43,20 @@
             </tr>
             </thead>
             <tbody>
-              {{-- タスク一覧を表示 --}}
-              @foreach($tasks as $task)
-                <tr>
-                  <td>{{ $task->title }}</td>
-                  <td>
-                    <span class="label {{ $task->status_class }}">{{ $task->status_label }}</span>
-                  </td>
-                  <td>{{ $task->formatted_due_date }}</td>
-                  <td><a href="#">編集</a></td>
-                </tr>
-              @endforeach
+            @foreach($tasks as $task)
+              <tr>
+                <td>{{ $task->title }}</td>
+                <td>
+                  <span class="label {{ $task->status_class }}">{{ $task->status_label }}</span>
+                </td>
+                <td>{{ $task->formatted_due_date }}</td>
+                <td><a href="#">編集</a></td>
+              </tr>
+            @endforeach
             </tbody>
           </table>
         </div>
       </div>
     </div>
   </div>
-</main>
-</body>
-</html>
+@endsection
