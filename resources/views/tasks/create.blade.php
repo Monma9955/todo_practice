18552c1f1e919e591011d44dfcd1642c3391499c
Monma_Takeshi <jemini0527@gmail.com>
2020-12-10 21:09:39 +0900

    JavaScriptライブラリ読み込み flatpickr
    カレンダーで日付を選択する機能が実装できるようにflatpickrのライブラリを読み込みました。

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -49,5 +49,15 @@
     </div>
   </div>
 </main>
+
+<script src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js"></script>
+<script src="https://npmcdn.com/flatpickr/dist/l10n/ja.js"></script>
+<script>
+  flatpickr(document.getElementById('due_date'), {
+    locale: 'ja',
+    dateFormat: "Y/m/d",
+    minDate: new Date()
+  });
+</script>
 </body>
 </html>
