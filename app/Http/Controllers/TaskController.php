d4c694caa3917c8583772129057cb224774b5b21
Monma_Takeshi <jemini0527@gmail.com>
2020-12-12 14:20:07 +0900

    createアクション作成 TaskController
    TaskControllerにcreateアクションを作成。
    requestとは別にcurrent folderのIDもリレーションに則って保存ができるようにしました。
    保存成功後のリダイレクト先はfolder idのindexに設定。

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Folder;
 use App\Models\Task;
+use App\Http\Requests\CreateTask;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -32,6 +33,23 @@ class TaskController extends Controller
     {
         return view('tasks/create', [
             'folder_id' => $id
+        ]);
+    }
+
+    public function create(int $id, CreateTask $request)
+    {
+        $current_folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+
+        // current_folderのタスクとしてテーブルに保存
+        $current_folder->tasks()->save($task);
+
+        // 作成したタスクの格納されているフォルダーのindexにリダイレクト
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id
         ]);
     }
 }
