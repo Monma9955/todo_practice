15299692b550da4fc18dff1413f688ed6588ef77
Monma_Takeshi <jemini0527@gmail.com>
2020-12-14 23:57:46 +0900

    タスク編集機能へのルーティング web.php
    web.phpにタスク編集機能へのルーティングを記載しました。

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.
 Route::post('/folders/create', 'FolderController@create');
 Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
 Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+Route::get('/folders/{id}/tasks/edit', 'TaskController@showEditForm')->name('tasks.edit');
+Route::post('/folders/{id}/tasks/edit', 'TaskController@edit');
