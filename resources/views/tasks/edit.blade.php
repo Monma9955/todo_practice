42b8c5e0ea517118f4a45ac059f1f4d5d5cd8d1c
Monma_Takeshi <jemini0527@gmail.com>
2020-12-15 08:46:36 +0900

    taskのeditビュー作成
    edit.blade.phpを作成。
    最初のページ表示ではDBに既に保存した値がプレースホルダーに表示され、バリデーションがかかった際はold関数で送信ボタンを押す前の値が表示されるようにしました。

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,73 @@
+@extends('layout')
+
+@section('styles')
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+  <link rel="stylesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
+@endsection
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="col col-md-offset-3 col-md-6">
+        <nav class="panel panel-default">
+          <div class="panel-heading">タスクを編集する</div>
+          <div class="panel-body">
+            @if($errors->any())
+              <div class="alert alert-danger">
+                @foreach($errors->all() as $message)
+                  <p>{{ $message }}</p>
+                @endforeach
+              </div>
+            @endif
+            <form
+                action="{{ route('tasks.edit', ['id' => $task->folder_id, 'task_id' => $task->id]) }}"
+                method="POST"
+            >
+              @csrf
+              <div class="form-group">
+                <label for="title">タイトル</label>
+                {{-- 直前の入力値がない場合(ページを最初に表示した時)にはDBにあるtitleがセットされる --}}
+                <input type="text" class="form-control" name="title" id="title"
+                       value="{{ old('title') ?? $task->title }}" />
+              </div>
+              <div class="form-group">
+                <label for="status">状態</label>
+                <select name="status" id="status" class="form-control">
+                  {{-- 配列定数STATUSをforeachでループしてoption要素を取り出している --}}
+                  @foreach(\App\Models\Task::STATUS as $key => $val)
+                    <option
+                        value="{{ $key }}"
+                        {{ $key == old('status', $task->status) ? 'selected' : '' }}
+                    >
+                      {{ $val['label'] }}
+                    </option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="due_date">期限</label>
+                <input type="text" class="form-control" name="due_date" id="due_date"
+                       value="{{ old('due_date') ?? $task->formatted_due_date }}" />
+              </div>
+              <div class="text-right">
+                <button type="submit" class="btn btn-primary">送信</button>
+              </div>
+            </form>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@section('scripts')
+  <script src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js"></script>
+  <script src="https://npmcdn.com/flatpickr/dist/l10n/ja.js"></script>
+  <script>
+    flatpickr(document.getElementById('due_date'), {
+      locale: 'ja',
+      dateFormat: "Y/m/d",
+      minDate: new Date()
+    });
+  </script>
+@endsection
