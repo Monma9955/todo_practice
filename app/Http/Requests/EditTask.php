afe113589893241a658048f81477ffe9929f3100
Monma_Takeshi <jemini0527@gmail.com>
2020-12-16 23:59:18 +0900

    EditTaskの内容にコメント追加
    EditTask内で初めて実装する内容にコメントで説明を追加しました。

--- app/Http/Requests/EditTask.php
+++ app/Http/Requests/EditTask.php
@@ -6,13 +6,16 @@ use App\Models\Task;
 use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
+// 親クラスとしてCreateTaskを継承
 class EditTask extends CreateTask
 {
     public function rules()
     {
+        // CreateTaskのルールを継承
         $rule = parent::rules();
 
-        $status_rule _ Rule::in(array_keys(Task::STATUS));
+        // 状態の入力値が許可リストに含まれているかRule::inで検証
+        $status_rule = Rule::in(array_keys(Task::STATUS));
 
         return $rule + [
             'status' => 'required|', $status_rule,
@@ -23,6 +26,7 @@ class EditTask extends CreateTask
     {
         $attributes = parent::attributes();
 
+        // 親クラスCreateTaskのattributesメソッドの結果と合体した結果を返却
         return $attributes + [
             'status' => '状態',
         ];
@@ -32,11 +36,13 @@ class EditTask extends CreateTask
     {
         $messages = parent::messages();
 
+        // 状態のラベル名を取り出し
         $status_labels = array_map(function($item) {
             return $item['label'];
         }, Task::STATUS);
 
-        $status_labels = imploge('、', $status_labels);
+        // 状態の各ラベル名に句読点をくっつける
+        $status_labels = implode('、', $status_labels);
 
         return $messages + [
             'status.in' => ':attribute には ', $status_labels, ' のいずれかを指定してください。',
