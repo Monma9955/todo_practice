542749dcedae56a31e96b979e84db17e0908e084
Monma_Takeshi <jemini0527@gmail.com>
2020-12-20 23:06:31 +0900

    database.php修正 testingDB明示
    envファイルで明示的にテストDB名を定義したので、database.phpをそれに合わせて修正しました。

--- database/migrations/2020_12_19_092624_add_user_id_to_folders.php
+++ database/migrations/2020_12_19_092624_add_user_id_to_folders.php
@@ -14,7 +14,10 @@ class AddUserIdToFolders extends Migration
     public function up()
     {
         Schema::table('folders', function (Blueprint $table) {
-            //
+            $table->integer('user_id')->unsigned();
+
+            // 外部キー設定
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -26,7 +29,7 @@ class AddUserIdToFolders extends Migration
     public function down()
     {
         Schema::table('folders', function (Blueprint $table) {
-            //
+            $table->dropColumn('user_id');
         });
     }
 }
