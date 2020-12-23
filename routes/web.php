c9bcd4aa9709af3cf5c4f4cc6df9f492efc6db4d
Monma_Takeshi <jemini0527@gmail.com>
2020-12-23 23:53:24 +0900

    ホームページ(トップページ)の作成 ルーティング
    トップページの作成。
    まずはルーティングでドメインに直アクセスした際にトップページが表示されるように設定しました。

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
