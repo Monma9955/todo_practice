fbb62328fd139a13b00b5100f04a4f69f5e5f575
Monma_Takeshi <jemini0527@gmail.com>
2020-12-24 23:30:11 +0900

    認証機能へのルーティング web.php
    web.phpに認証機能へのルーティングを記載しました。

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
