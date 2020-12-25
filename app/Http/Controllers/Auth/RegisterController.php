eb6634ce774f3817cd4f498a7c44eee6248f1702
Monma_Takeshi <jemini0527@gmail.com>
2020-12-26 02:32:13 +0900

    バリデーションエラーメッセージの日本語化
    RegisterController.phpでuserの項目名を日本語化しました。

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,10 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ], [], [
+            'name' => 'ユーザー名',
+            'email' => 'メールアドレス',
+            'password' => 'パスワード',
         ]);
     }
 
