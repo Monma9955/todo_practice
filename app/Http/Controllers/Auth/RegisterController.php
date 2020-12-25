78435916eff61e61d12ad85e75e91a85bae26f29
Monma_Takeshi <jemini0527@gmail.com>
2020-12-26 02:35:44 +0900

    Revert "バリデーションエラーメッセージの日本語化"
    This reverts commit eb6634ce774f3817cd4f498a7c44eee6248f1702.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,10 +53,6 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ], [], [
-            'name' => 'ユーザー名',
-            'email' => 'メールアドレス',
-            'password' => 'パスワード',
         ]);
     }
 
