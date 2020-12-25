fafb194c0a17a267f1e281a995c2ae586b5d1b33
Monma_Takeshi <jemini0527@gmail.com>
2020-12-25 17:02:24 +0900

    laravel ui 認証機能実装
    php artisan ui vue --authコマンドでLaravel uiの認証機能のセットを作成しました。

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name(
 Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
