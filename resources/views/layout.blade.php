ab444150b5551871fbd548d7881645e91a8f767f
Monma_Takeshi <jemini0527@gmail.com>
2020-12-27 01:15:23 +0900

    ログイン分岐　ヘッダー
    ヘッダーメニューをログイン・非ログイン時で条件分岐するように設定。
    非ログイン時にはログインか会員登録ができるようにしました。

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,6 +12,21 @@
 <header>
   <nav class="my-navbar">
     <a class="my-navbar-brand" href="/">ToDo App</a>
+    <div class="my-navbar-control">
+      {{-- ログインしているかどうかで分岐 --}}
+      @if(Auth::check())
+        <span class="my-navbar-item">ようこそ、 {{ Auth::user()->name }}さん</span>
+        |
+        <a href="#" id="logout" class="my-navbar-item">ログアウト</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          @csrf
+        </form>
+      @else
+        <a class="my-navbar-item" href="{{ route('login') }}">ログイン</a>
+        |
+        <a class="my-navbar-item" href="{{ route('register') }}">会員登録</a>
+      @endif
+    </div>
   </nav>
 </header>
 <main>
