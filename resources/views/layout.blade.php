31a7ae781501507c3397bc0597979534f48d9c6d
Monma_Takeshi <jemini0527@gmail.com>
2020-12-27 18:42:03 +0900

    ログアウト処理のJavaScript修正
    ログアウト処理のJavaScriptで引数エラーが出たため対処。「,」を入れるべきところが誤って「.」になっていたため修正しました。

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -35,7 +35,7 @@
 {{-- ログアウトリンクのクリックでその下のフォームからlogoutのURLにPOSTリクエストを送信できるよう設定 --}}
 @if(Auth::check())
   <script>
-    document.getElementById('logout').addEventListener('click'. function(event) {
+    document.getElementById('logout').addEventListener('click', function(event) {
       event.preventDefault();
       document.getElementById('logout-form').submit();
     });
