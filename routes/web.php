e4510f82ee1138597796bbc8dc02c96efdf3b2d0
Monma_Takeshi <jemini0527@gmail.com>
2020-12-27 17:58:35 +0900

    ページ認証 web.php
    web.phpに定義していたルーティングにauthミドルウェアを使用し、ログインしないとアクセスできないようにしました。

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,19 @@
 <?php
 
-Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
-Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
-Route::post('/folders/create', 'FolderController@create');
-Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', 'TaskController@create');
-Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+Route::group(['middleware' => 'auth'], function () {
+  Route::get('/', 'HomeController@index')->name('home');
+  Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+  Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+  Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+  Route::post('/folders/create', 'FolderController@create');
+
+  Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+  Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+
+  Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+  Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+});
+
+Auth::routes();
