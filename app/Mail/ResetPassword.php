40625ab9adce6556d51778bb34be191a5ca0a23c
Monma_Takeshi <jemini0527@gmail.com>
2020-12-28 11:06:50 +0900

    Mailableクラス作成 ResetPassword.php
    MailableクラスのResetPassword.phpを作成。
    トークン・メールの件名のパラメータをこれから作成するpassword-resetテンプレートに渡すよう設定しました。

---
+++ app/Mail/ResetPassword.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ResetPassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $token;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param $token
+     */
+    public function __construct($token)
+    {
+        $this->token = $token; //トークンを受け取りメールテンプレートに渡す
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->subject('パスワード再設定') //件名を設定
+            ->view('mail.password-reset'); //メールテンプレートとしてpassword-reset.blade.phpを指定
+    }
+}
