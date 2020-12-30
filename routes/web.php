1719e7d8dfc378a5b473e7cec13186de6853da5c
Monma_Takeshi <jemini0527@gmail.com>
2020-12-31 00:29:14 +0900

    認可処理 ミドルウェアから呼び出して使用
    ログインユーザーとフォルダーが一致している場合の認可処理を{folder}を使うルーティング全てに適用。
    canミドルウェアを使用し、FolderPolicy.phpのviewメソッドの真偽で認可するようにしました。

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,19 @@
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', 'HomeController@index')->name('home');
 
-    Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
-
     Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
     Route::post('/folders/create', 'FolderController@create');
 
-    Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-    Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
+    // canミドルウェアでview認可処理にルートパラメーター{folder}を渡す
+    Route::group(['middleware' => 'can:view,folder'], function () {
+        Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
 
-    Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-    Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
+        Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+        Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
+
+        Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+        Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
+    });
 });
 
 Auth::routes();
